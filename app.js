32bafe6ce25373004cde52d721c0b0b443c254b4
Nurhadi Permana <hadistylecool@gmail.com>
2023-05-30 14:16:34 +0700

    API Express

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const productRouter = require('./app/product/routes');
+const productRouterV2 = require('./app/product_v2/routes');
+const logger = require('morgan');
 
-app.get('/', (req, res) => {
-  res.sendFile('./index.html', { root: __dirname });
-});
 
-app.get('/about', (req, res) => {
-  // res.send('<h1>Ini adalah Halaman About</h1>');
-  res.sendFile('./about.html', { root: __dirname });
-});
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/v1', productRouter);
+app.use('/api/v2', productRouterV2);
+app.use((req, res, next) => {
+  res.status(404);
+  res.send({
+    status: 'failed',
+    message: 'Resource ' + req.originalUrl + ' Not Found'
+  })
+})
+app.listen(3000, () => console.log('Server: http://localhost:3000'))
 
-app.get('/contact', (req, res) => {
-  // res.send('<h1>Ini adalah Halaman Contact</h1>');
-  res.sendFile('./contact.html', { root: __dirname });
-});
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-app.use('/', (req, res) => {
-  res.status('404');
-  res.send('<h1>404</h1>');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile('./index.html', { root: __dirname });
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.get('/about', (req, res) => {
+//   // res.send('<h1>Ini adalah Halaman About</h1>');
+//   res.sendFile('./about.html', { root: __dirname });
+// });
+
+// app.get('/contact', (req, res) => {
+//   // res.send('<h1>Ini adalah Halaman Contact</h1>');
+//   res.sendFile('./contact.html', { root: __dirname });
+// });
+
+// app.use('/', (req, res) => {
+//   res.status('404');
+//   res.send('<h1>404</h1>');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
